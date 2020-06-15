79da538216a2b4646c34873e65b3e2413f57acbb
Erin Claudio <eclaudio@gmail.com>
2020-06-15 12:14:45 -0400

    blog repr test positive

---
+++ blog2/tests/unit/blog_test.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from blog import Blog
+
+
+class BlogTest(TestCase):
+    def test_create_blog(self):
+        b = Blog('Test title', 'Test author')
+
+        self.assertEqual('Test title', b.title)
+        self.assertEqual('Test author', b.author)
+        self.assertListEqual([], b.posts)
+
+    def test_repr(self):
+        b = Blog('Test title', 'Test author')
+        b.posts = ['test post']
+        b2 = Blog('My Day', 'Rolf')
+        b2.posts = ['test post', 'test 2']
+
+
+        self.assertEqual(b.__repr__(), 'Test title by Test author (1 post )')
